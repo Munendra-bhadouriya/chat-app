9ab6f4a95ea0ee2b514345704fc9dac3139ee02f
Munendra-bhadouriya <munendra.connect@gmail.com>
2025-04-25 00:27:32 +0530

    added firebase credentials in env

--- lets-chat-main/server/config/firebase-config.js
+++ lets-chat-main/server/config/firebase-config.js
@@ -1,10 +1,7 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
-import fs from 'fs';
-
-const serviceAccountKey = JSON.parse(fs.readFileSync('./serviceAccountKey.json', 'utf-8'));
-
+const serviceAccountKey = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 const app = initializeApp({
   credential: cert(serviceAccountKey),
